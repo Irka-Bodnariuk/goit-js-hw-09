ed60cb9a6585a0f5493679af0eaadb2721320182
Irka-Bodnariuk <104754511+Irka-Bodnariuk@users.noreply.github.com>
2022-09-30 09:43:05 +0000

    Deploying to gh-pages from @ Irka-Bodnariuk/goit-js-hw-09@5cd54e83ad7bcdfa1b3c3c17fd20828d24bd8846 🚀

---
+++ 01-color-switcher.64ebb986.js
@@ -0,0 +1,2 @@
+const t={stasrtBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;t.stasrtBtn.addEventListener("click",(function(){isActive=!0,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.stasrtBtn.setAttribute("disabled",""),t.stopBtn.toggleAttribute("disabled","")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.stasrtBtn.toggleAttribute("disabled",""),t.stopBtn.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.64ebb986.js.map
