5cd54e83ad7bcdfa1b3c3c17fd20828d24bd8846
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-09-30 12:42:36 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,26 @@
+const refs = {
+  stasrtBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let intervalId = null;
+
+refs.stasrtBtn.addEventListener('click', onClickStartBtn);
+refs.stopBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  isActive = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.stasrtBtn.setAttribute('disabled', '');
+  refs.stopBtn.toggleAttribute('disabled', '');
+}
+function onClickStopBtn() {
+  clearInterval(intervalId);
+  refs.stasrtBtn.toggleAttribute('disabled', '');
+  refs.stopBtn.setAttribute('disabled', '');
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
